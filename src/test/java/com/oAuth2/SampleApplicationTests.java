fb010c30019f47f00cdbe9ac9546db8dd0067605
chandan30 <chandan98sharma111@gmail.com>
2025-09-01 22:38:20 +0530

    initial commit

---
+++ src/test/java/com/oAuth2/SampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oAuth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
